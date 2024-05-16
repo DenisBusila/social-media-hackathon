84e79b0f056f857e29e0b87960e1ec4c2cc178e5
DenisBusila <busiladenisg@gmail.com>
2024-05-17 00:05:41 +0300

    Add files via upload
    added a feed for the website, still not linked to the other parts.

---
+++ index.js
@@ -0,0 +1,74 @@
+const menuItems = document.querySelectorAll('.menu-item');
+const theme = document.querySelector('#theme');
+const fontSize = document.querySelectorAll('.choose-size span');
+var root = document.querySelector(':root');
+const colorPalette = document.querySelectorAll('.choose-color span');
+const Bg1 = document.querySelector('.bg-1');
+const Bg2 = document.querySelector('.bg-2');
+const Bg3 = document.querySelector('.bg-3');
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove('active');})}
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem();
+        item.classList.add('active');
+        if(item.id != 'notifications') {
+            document.querySelector('.notifications-popup').
+            style.display = 'none';
+        } else {
+            document.querySelector('.notifications-popup').
+            style.display = 'block';
+            document.querySelector('#notifications .notification-count').
+            style.display = 'none';}})})
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(user => {
+        let name = user.querySelector('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1) {
+            user.style.display = 'flex'; 
+        } else {
+            user.style.display = 'none';}})}
+messageSearch.addEventListener('keyup', searchMessage);
+messageNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messageNotification.querySelector('.notification-count').style.display = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);})
+const openThemeModal = () => {
+    themeModal.style.display = 'grid';}
+const closeThemeModal = (e) => {
+    if(e.target.classList.contains('customize-theme')) {
+        themeModal.style.display = 'none';}}
+themeModal.addEventListener('click', closeThemeModal);
+theme.addEventListener('click', openThemeModal);
+const removeSizeSelectors = () => {
+    fontSize.forEach(size => {
+        size.classList.remove('active');})}
+fontSize.forEach(size => { 
+   size.addEventListener('click', () => {
+        removeSizeSelectors();
+        let fontSize;
+        size.classList.toggle('active');
+        if(size.classList.contains('font-size-1')) { 
+            fontSize = '10px';
+            root.style.setProperty('----sticky-top-left', '5.4rem');
+            root.style.setProperty('----sticky-top-right', '5.4rem');
+        } else if(size.classList.contains('font-size-2')) { 
+            fontSize = '13px';
+            root.style.setProperty('----sticky-top-left', '5.4rem');
+            root.style.setProperty('----sticky-top-right', '-7rem');
+        } else if(size.classList.contains('font-size-3')) {
+            fontSize = '16px';
+            root.style.setProperty('----sticky-top-left', '-2rem');
+            root.style.setProperty('----sticky-top-right', '-17rem');
+        } else if(size.classList.contains('font-size-4')) {
+            fontSize = '19px';
+            root.style.setProperty('----sticky-top-left', '-5rem');
+            root.style.setProperty('----sticky-top-right', '-25rem');
+        } else if(size.classList.contains('font-size-5')) {
+            fontSize = '22px';
+            root.style.setProperty('----sticky-top-left', '-12rem');
+            root.style.setProperty('----sticky-top-right', '-35rem');}
+        document.querySelector('html').style.fontSize = fontSize;})})
